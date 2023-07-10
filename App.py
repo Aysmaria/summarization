3d05c934fa98f7f044401b856385f605c8d94bd0
Aysmaria <56933749+Aysmaria@users.noreply.github.com>
2023-07-10 15:16:19 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def load_data(file_path):
-    return pd.read_excel(file_path)
+    return pd.read_csv(file_path)
 
 def save_data(file_path, data):
     data.to_excel(file_path, index=False)
