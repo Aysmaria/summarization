fb2a88a27bddff28f71e956cd2ad3102296b1e65
Aysmaria <56933749+Aysmaria@users.noreply.github.com>
2023-07-13 17:53:55 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ import streamlit as st
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
+    return pd.read_excel(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
 
