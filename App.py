c101386b3f4aaafb38975a9e5bb23dc13d6bca42
Aysmaria <56933749+Aysmaria@users.noreply.github.com>
2023-07-16 20:55:16 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -1,105 +1,200 @@
-import google_auth_httplib2
-import httplib2
-import pandas as pd
+import os
 import streamlit as st
+import pandas as pd
 from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import HttpRequest
+import gspread
+from gspread_dataframe import set_with_dataframe
+import openai
+import numpy as np
+from transformers import GPT2TokenizerFast
+# from sentence_transformers import SentenceTransformer
+import datetime
 
-SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1QlPTiVvfRM82snGN6LELpNkOwVI1_Mp9J9xeJe-QoaA"
-SHEET_NAME = "test_data_sample.xlsx_gpt-3"
-GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
 
-@st.experimental_singleton()
-def connect_to_gsheet():
-    # Create a connection object.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[SCOPE],
-    )
+# This should be under `if check_password():`
 
-    # Create a new Http() object for every request
-    def build_request(http, *args, **kwargs):
-        new_http = google_auth_httplib2.AuthorizedHttp(
-            credentials, http=httplib2.Http()
+    # Read database on Google sheet
+    ###############################
+# @st.cache_resource
+def access_sheet(sheet_name):
+        '''
+        Access the Google's spreadsheet.
+        '''
+        # From local computer
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = service_account.Credentials.from_service_account_info(
+                    st.secrets["gcp_service_account"], scopes = scope)
+        gc = gspread.authorize(credentials)
+        sheet = gc.open('test_data_sample.xlsx_gpt-3').worksheet(sheet_name)
+        print("Opened file")
+        return sheet
+
+
+# @st.cache_data
+def get_data():
+        '''
+        Read the biographical information from the Google sheet
+        Returns
+        -------
+        df : Pandas dataframe
+            Contains columns 'section', 'content', 'embeddings' & 'num_tokens'
+        '''
+        sheet = access_sheet('Test')
+        data = sheet.get_all_values()
+        df = pd.DataFrame(data[1:], columns=['text', 'original_summary', 'topic', 'title', 'dataset_name',	'generated_summary','params', 'bert_score_text_generated_summary', 'bert_score_summary_generated_summary', 'rouge1_scores_text_generated_summary',	'rouge1_scores_summary_generated_summary',	'rouge2_scores_text_generated_summary',	'rouge2_scores_summary_generated_summary',	'rougeL_scores_text_generated_summary',	'rougeL_scores_summary_generated_summary',	'model_name'])
+
+        # df['new_column_name'] = None
+        print(df)
+        # set_with_dataframe(sheet, df)
+        return df
+
+def save_data(sheet_name, data):
+    '''
+    Save the data to a Google sheet.
+    '''
+    sheet = access_sheet(sheet_name)
+    set_with_dataframe(sheet, data)
+
+'''
+def load_data(file_path):
+    return pd.read_excel(file_path)
+
+
+def save_data(file_path, data):
+    data.to_excel(file_path, index=False)
+'''
+
+# data = get_data('test_params_combos.xlsx')
+data = get_data()
+st.title("Text Summarization Analysis")
+
+if 'selected_index' not in st.session_state:
+    st.session_state['selected_index'] = 0
+
+selected_index = st.session_state['selected_index']
+# Check if all texts have been processed
+if selected_index >= len(data):
+    st.write("All texts have been processed. Algorithm finished.")
+    st.stop()
+
+st.markdown(f"**Text {selected_index + 1} of {len(data)}**")
+
+selected_row = data.iloc[selected_index]
+
+st.markdown(f"**Category**\n\n{selected_row['topic']}")
+
+st.markdown(f"**Original Text:**\n\n{selected_row['text']}")
+
+st.markdown(f"**Original Summary:**\n\n{selected_row['summary']}")
+
+st.markdown(f"**Generated Summary:**\n\n{selected_row['generated_summary']}")
+
+with st.sidebar:
+    st.markdown("Select criteria")
+    criteria = ['Readability', 'Informativeness', 'Fluency', 'Conciseness', 'Factual correctness']
+    criteria_info = {
+        'Readability': 'It measures how well the summary is fluent and grammatical.',
+        'Informativeness': 'It measures how well the summary contains the gist of the original input.',
+        'Fluency': 'It measures how well the summary is consistent with human language habits.',
+        'Conciseness': 'It measures whether the summary is simple and easy to understand (less redundancy)',
+        'Factual correctness': 'It indicates whether the facts described in the summary are consistent with the original document, which is the most critical factor affecting the usability of the summary.'
+
+    }
+
+    for criterion in criteria:
+        if criterion not in st.session_state:
+            st.session_state[criterion] = 0
+
+    for criterion in criteria:
+        st.session_state[criterion] = st.sidebar.slider(
+            criterion,
+            0,
+            5,
+            st.session_state[criterion],
+            help=criteria_info[criterion]
         )
-        return HttpRequest(new_http, *args, **kwargs)
 
-    authorized_http = google_auth_httplib2.AuthorizedHttp(
-        credentials, http=httplib2.Http()
-    )
-    service = build(
-        "sheets",
-        "v4",
-        requestBuilder=build_request,
-        http=authorized_http,
-    )
-    gsheet_connector = service.spreadsheets()
-    return gsheet_connector
+categories = [
+    'Undefined',
+    'Politik',
+    'Wirtschaft',
+    'Panorama',
+    'Sport',
+    'Reise',
+    'Auto',
+    'Digital',
+    'Geld/Finanzen'
+]
 
+categories_info = {
+    'Undefined': "it's hard to identify",
+    'Politik': 'Politische Themen und Entscheidungen, Migration, Asylrecht, Grenzsicherheit, Europäischen Union',
+    'Wirtschaft': 'Die wirtschaftliche Aspekte, die Wechselwirkungen zwischen Wirtschaft, Politik und Verbrauchern. Wirtschaftlichen Entscheidungen, Marktbedingungen und politischen Strategien',
+    'Panorama': 'Aktuelle Erignisse, Unterhaltungs- & Klatschwert',
+    'Sport': 'Sportberichterstattung, Sportarten',
+    'Reise': 'Landschaftliche Aspekte, Reisebericht, Reiseempfehlung, Mobilität, Verkehrsmittelnutzung, Tipps und Regeln für Reisen',
+    'Auto': 'Automobilindustrie, Nachhaltigkeit, Straßerverkehr, Sicherheit',
+    'Digital': 'Technische Innovatonen, Digitale Geräte, Software, Online-Dienste, KI, Nutzung von Technologien',
+    'Geld/Finanzen': 'Geldanlagen, Währungskrise, das Verhalten von Menschen im Zusammenhang mit Geld, Inflation, Preisen, Löhnen',
+}
 
-def get_data(gsheet_connector) -> pd.DataFrame:
-    values = (
-        gsheet_connector.values()
-        .get(
-            spreadsheetId=SPREADSHEET_ID,
-            range=f"{SHEET_NAME}!A:E",
-        )
-        .execute()
-    )
+if 'category' not in st.session_state:
+    st.session_state['category'] = categories[0]
 
-    df = pd.DataFrame(values["values"])
-    df.columns = df.iloc[0]
-    df = df[1:]
-    return df
-
-
-def add_row_to_gsheet(gsheet_connector, row) -> None:
-    gsheet_connector.values().append(
-        spreadsheetId=SPREADSHEET_ID,
-        range=f"{SHEET_NAME}!A:E",
-        body=dict(values=row),
-        valueInputOption="USER_ENTERED",
-    ).execute()
-
-
-st.set_page_config(page_title="Bug report", page_icon="🐞", layout="centered")
-
-st.title("🐞 Bug report!")
-
-gsheet_connector = connect_to_gsheet()
-
-st.sidebar.write(
-    f"This app shows how a Streamlit app can interact easily with a [Google Sheet]({GSHEET_URL}) to read or store data."
+st.session_state['category'] = st.sidebar.radio(
+    'Choose a category',
+    categories,
+    index=categories.index(st.session_state['category'])
 )
 
+st.sidebar.markdown(categories_info[st.session_state['category']])
 
-form = st.form(key="annotation")
+if 'comment' not in st.session_state:
+    st.session_state['comment'] = ''
 
-with form:
-    cols = st.columns((1, 1))
-    author = cols[0].text_input("Report author:")
-    bug_type = cols[1].selectbox(
-        "Bug type:", ["Front-end", "Back-end", "Data related", "404"], index=2
-    )
-    comment = st.text_area("Comment:")
-    cols = st.columns(2)
-    date = cols[0].date_input("Bug date occurrence:")
-    bug_severity = cols[1].slider("Bug severity:", 1, 5, 2)
-    submitted = st.form_submit_button(label="Submit")
+st.session_state['comment'] = st.sidebar.text_input("Comment", st.session_state['comment'])
+
+if st.button("Show Additional Information"):
+    info_columns = [
+        "title",
+        "dataset",
+        "params",
+        "model",
+        "temperature",
+        "max_tokens",
+        "topic",
+        "url",
+        "date",
+        "rouge1_text_to_generated",
+        "rouge2_text_to_generated",
+        "rougeL_text_to_generated",
+        "rouge1_summary_to_generated",
+        "rouge2_summary_to_generated",
+        "rougeL_summary_to_generated",
+        "bert_text_to_generated",
+        "bert_summary_to_generated"]
+
+    for column in info_columns:
+        if column in selected_row:
+            st.write(f"{column}: {selected_row[column]}")
+
+if st.button("Next"):
+    scores = [st.session_state[criterion] for criterion in criteria]
+    for i, criterion in enumerate(criteria):
+        # UPDATE DATA
+        data.at[selected_index, criterion] = scores[i]
+    data.at[selected_index, 'Comment'] = st.session_state['comment']
+    data.at[selected_index, 'Category'] = st.session_state['category']
+
+    save_data('Test', data)  # 'Test' should be the name of your Google Spreadsheet.
 
 
-if submitted:
-    add_row_to_gsheet(
-        gsheet_connector,
-        [[author, bug_type, comment, str(date), bug_severity]],
-    )
-    st.success("Thanks! Your bug was recorded.")
-    st.balloons()
+    st.session_state['selected_index'] = (selected_index + 1) % len(data)
 
-expander = st.expander("See all records")
-with expander:
-    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
-    st.dataframe(get_data(gsheet_connector))
+    for criterion in criteria:
+        st.session_state[criterion] = 0
+    st.session_state['comment'] = ''
+    st.session_state['category'] = categories[0]
+    st.experimental_rerun()
