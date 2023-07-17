53771b09f76b5e76c866cc864187fd7675f050d1
Aysmaria <56933749+Aysmaria@users.noreply.github.com>
2023-07-17 12:00:04 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -103,10 +103,9 @@ def save_data(file_path, data):
 # Ask for the user's name at the start of the session
 if 'user_id' not in st.session_state:
     st.session_state['user_id'] = st.text_input('Please enter your name to begin')
+else:
 
-    # Prevent the rest of the app from running until the user_id is set
-    if st.session_state['user_id'] == '':
-        st.stop()
+
 
     # Create a new spreadsheet for this user if it doesn't exist
     try:
@@ -117,141 +116,141 @@ if 'user_id' not in st.session_state:
 
 
 
-# Load the data from the user's spreadsheet
-data = get_data(st.session_state['user_id'])
+    # Load the data from the user's spreadsheet
+    data = get_data(st.session_state['user_id'])
 
 
-st.title("Text Summarization Analysis")
+    st.title("Text Summarization Analysis")
 
-if 'selected_index' not in st.session_state:
-    st.session_state['selected_index'] = 0
+    if 'selected_index' not in st.session_state:
+        st.session_state['selected_index'] = 0
 
-selected_index = st.session_state['selected_index']
-# Check if all texts have been processed
-if selected_index >= len(data):
-    st.write("All texts have been processed. Algorithm finished.")
-    st.stop()
+    selected_index = st.session_state['selected_index']
+    # Check if all texts have been processed
+    if selected_index >= len(data):
+        st.write("All texts have been processed. Algorithm finished.")
+        st.stop()
 
-st.markdown(f"**Text {selected_index + 1} of {len(data)}**")
+    st.markdown(f"**Text {selected_index + 1} of {len(data)}**")
 
-selected_row = data.iloc[selected_index]
+    selected_row = data.iloc[selected_index]
 
-st.markdown(f"**Category**\n\n{selected_row['topic']}")
+    st.markdown(f"**Category**\n\n{selected_row['topic']}")
 
-st.markdown(f"**Original Text:**\n\n{selected_row['text']}")
+    st.markdown(f"**Original Text:**\n\n{selected_row['text']}")
 
-st.markdown(f"**Original Summary:**\n\n{selected_row['original_summary']}")
+    st.markdown(f"**Original Summary:**\n\n{selected_row['original_summary']}")
 
-st.markdown(f"**Generated Summary:**\n\n{selected_row['generated_summary']}")
+    st.markdown(f"**Generated Summary:**\n\n{selected_row['generated_summary']}")
 
-with st.sidebar:
-    st.markdown("Select criteria")
-    criteria = ['Readability', 'Informativeness', 'Fluency', 'Conciseness', 'Factual correctness']
-    criteria_info = {
-        'Readability': 'It measures how well the summary is fluent and grammatical.',
-        'Informativeness': 'It measures how well the summary contains the gist of the original input.',
-        'Fluency': 'It measures how well the summary is consistent with human language habits.',
-        'Conciseness': 'It measures whether the summary is simple and easy to understand (less redundancy)',
-        'Factual correctness': 'It indicates whether the facts described in the summary are consistent with the original document, which is the most critical factor affecting the usability of the summary.'
+    with st.sidebar:
+        st.markdown("Select criteria")
+        criteria = ['Readability', 'Informativeness', 'Fluency', 'Conciseness', 'Factual correctness']
+        criteria_info = {
+            'Readability': 'It measures how well the summary is fluent and grammatical.',
+            'Informativeness': 'It measures how well the summary contains the gist of the original input.',
+            'Fluency': 'It measures how well the summary is consistent with human language habits.',
+            'Conciseness': 'It measures whether the summary is simple and easy to understand (less redundancy)',
+            'Factual correctness': 'It indicates whether the facts described in the summary are consistent with the original document, which is the most critical factor affecting the usability of the summary.'
 
+        }
+
+        for criterion in criteria:
+            if criterion not in st.session_state:
+                st.session_state[criterion] = 0
+
+        for criterion in criteria:
+            st.session_state[criterion] = st.sidebar.slider(
+                criterion,
+                0,
+                5,
+                st.session_state[criterion],
+                help=criteria_info[criterion]
+            )
+
+    categories = [
+        'Undefined',
+        'Politik',
+        'Wirtschaft',
+        'Panorama',
+        'Sport',
+        'Reise',
+        'Auto',
+        'Digital',
+        'Geld/Finanzen'
+    ]
+
+    categories_info = {
+        'Undefined': "it's hard to identify",
+        'Politik': 'Politische Themen und Entscheidungen, Migration, Asylrecht, Grenzsicherheit, Europäischen Union',
+        'Wirtschaft': 'Die wirtschaftliche Aspekte, die Wechselwirkungen zwischen Wirtschaft, Politik und Verbrauchern. Wirtschaftlichen Entscheidungen, Marktbedingungen und politischen Strategien',
+        'Panorama': 'Aktuelle Erignisse, Unterhaltungs- & Klatschwert',
+        'Sport': 'Sportberichterstattung, Sportarten',
+        'Reise': 'Landschaftliche Aspekte, Reisebericht, Reiseempfehlung, Mobilität, Verkehrsmittelnutzung, Tipps und Regeln für Reisen',
+        'Auto': 'Automobilindustrie, Nachhaltigkeit, Straßerverkehr, Sicherheit',
+        'Digital': 'Technische Innovatonen, Digitale Geräte, Software, Online-Dienste, KI, Nutzung von Technologien',
+        'Geld/Finanzen': 'Geldanlagen, Währungskrise, das Verhalten von Menschen im Zusammenhang mit Geld, Inflation, Preisen, Löhnen',
     }
 
-    for criterion in criteria:
-        if criterion not in st.session_state:
+    if 'category' not in st.session_state:
+        st.session_state['category'] = categories[0]
+
+    st.session_state['category'] = st.sidebar.radio(
+        'Choose a category',
+        categories,
+        index=categories.index(st.session_state['category'])
+    )
+
+    st.sidebar.markdown(categories_info[st.session_state['category']])
+
+    if 'comment' not in st.session_state:
+        st.session_state['comment'] = ''
+
+    st.session_state['comment'] = st.sidebar.text_input("Comment", st.session_state['comment'])
+
+    if st.button("Show Additional Information"):
+        info_columns = [
+            "title",
+            "dataset",
+            "params",
+            "model",
+            "temperature",
+            "max_tokens",
+            "topic",
+            "url",
+            "date",
+            "rouge1_text_to_generated",
+            "rouge2_text_to_generated",
+            "rougeL_text_to_generated",
+            "rouge1_summary_to_generated",
+            "rouge2_summary_to_generated",
+            "rougeL_summary_to_generated",
+            "bert_text_to_generated",
+            "bert_summary_to_generated"]
+
+        for column in info_columns:
+            if column in selected_row:
+                st.write(f"{column}: {selected_row[column]}")
+
+    if st.button("Next"):
+        scores = [st.session_state[criterion] for criterion in criteria]
+        for i, criterion in enumerate(criteria):
+            # UPDATE DATA
+            data.at[selected_index, criterion] = scores[i]
+
+        data.at[selected_index, 'Comment'] = st.session_state['comment']
+        data.at[selected_index, 'Category'] = st.session_state['category']
+
+        # Add user_id to the row in the DataFrame
+        data.at[selected_index, 'User'] = st.session_state['user_id']
+
+        # Save this row of data to the user's own spreadsheet
+        save_data(st.session_state['user_id'], data)
+
+        st.session_state['selected_index'] = (selected_index + 1) % len(data)
+    
+        for criterion in criteria:
             st.session_state[criterion] = 0
-
-    for criterion in criteria:
-        st.session_state[criterion] = st.sidebar.slider(
-            criterion,
-            0,
-            5,
-            st.session_state[criterion],
-            help=criteria_info[criterion]
-        )
-
-categories = [
-    'Undefined',
-    'Politik',
-    'Wirtschaft',
-    'Panorama',
-    'Sport',
-    'Reise',
-    'Auto',
-    'Digital',
-    'Geld/Finanzen'
-]
-
-categories_info = {
-    'Undefined': "it's hard to identify",
-    'Politik': 'Politische Themen und Entscheidungen, Migration, Asylrecht, Grenzsicherheit, Europäischen Union',
-    'Wirtschaft': 'Die wirtschaftliche Aspekte, die Wechselwirkungen zwischen Wirtschaft, Politik und Verbrauchern. Wirtschaftlichen Entscheidungen, Marktbedingungen und politischen Strategien',
-    'Panorama': 'Aktuelle Erignisse, Unterhaltungs- & Klatschwert',
-    'Sport': 'Sportberichterstattung, Sportarten',
-    'Reise': 'Landschaftliche Aspekte, Reisebericht, Reiseempfehlung, Mobilität, Verkehrsmittelnutzung, Tipps und Regeln für Reisen',
-    'Auto': 'Automobilindustrie, Nachhaltigkeit, Straßerverkehr, Sicherheit',
-    'Digital': 'Technische Innovatonen, Digitale Geräte, Software, Online-Dienste, KI, Nutzung von Technologien',
-    'Geld/Finanzen': 'Geldanlagen, Währungskrise, das Verhalten von Menschen im Zusammenhang mit Geld, Inflation, Preisen, Löhnen',
-}
-
-if 'category' not in st.session_state:
-    st.session_state['category'] = categories[0]
-
-st.session_state['category'] = st.sidebar.radio(
-    'Choose a category',
-    categories,
-    index=categories.index(st.session_state['category'])
-)
-
-st.sidebar.markdown(categories_info[st.session_state['category']])
-
-if 'comment' not in st.session_state:
-    st.session_state['comment'] = ''
-
-st.session_state['comment'] = st.sidebar.text_input("Comment", st.session_state['comment'])
-
-if st.button("Show Additional Information"):
-    info_columns = [
-        "title",
-        "dataset",
-        "params",
-        "model",
-        "temperature",
-        "max_tokens",
-        "topic",
-        "url",
-        "date",
-        "rouge1_text_to_generated",
-        "rouge2_text_to_generated",
-        "rougeL_text_to_generated",
-        "rouge1_summary_to_generated",
-        "rouge2_summary_to_generated",
-        "rougeL_summary_to_generated",
-        "bert_text_to_generated",
-        "bert_summary_to_generated"]
-
-    for column in info_columns:
-        if column in selected_row:
-            st.write(f"{column}: {selected_row[column]}")
-
-if st.button("Next"):
-    scores = [st.session_state[criterion] for criterion in criteria]
-    for i, criterion in enumerate(criteria):
-        # UPDATE DATA
-        data.at[selected_index, criterion] = scores[i]
-
-    data.at[selected_index, 'Comment'] = st.session_state['comment']
-    data.at[selected_index, 'Category'] = st.session_state['category']
-
-    # Add user_id to the row in the DataFrame
-    data.at[selected_index, 'User'] = st.session_state['user_id']
-
-    # Save this row of data to the user's own spreadsheet
-    save_data(st.session_state['user_id'], data)
-
-    st.session_state['selected_index'] = (selected_index + 1) % len(data)
-
-    for criterion in criteria:
-        st.session_state[criterion] = 0
-    st.session_state['comment'] = ''
-    st.session_state['category'] = categories[0]
-    st.experimental_rerun()
+        st.session_state['comment'] = ''
+        st.session_state['category'] = categories[0]
+        st.experimental_rerun()
