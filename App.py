ba7d1c6a475919bfefab587c09b6ac48a95d30ca
Aysmaria <56933749+Aysmaria@users.noreply.github.com>
2023-07-26 00:27:06 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -274,11 +274,15 @@ if user_name:
                 st.session_state['selected_index'] += 1
             else:
                 st.session_state['all_processed'] = True  # Update all_processed when all texts have been processed
-                st.write("All texts have been processed :)  Thank you for participation 🩷")
-                st.balloons()  # Streamlit balloons
-                st.stop()
-
-                
+                # Ask for user's opinion
+                user_opinion = st.text_input("Please enter your opinion about the data processing:")
+                submit_button = st.button("Submit Opinion")
+                if submit_button:
+                    user_data.at[selected_index, 'User Opinion'] = user_opinion if user_opinion else "No opinion given"
+                    save_data("sorted_FINAL_DATA", st.session_state['user_name'], user_data)
+                    st.write("All texts have been processed :)  Thank you for participation 🩷")
+                    st.balloons()  # Streamlit balloons
+                    st.stop()
 
             # Reset the scores, comment, and category in the session_state
             for criterion in criteria:
