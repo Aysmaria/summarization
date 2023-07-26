c5aebe0fb9210f0139e8ff1261ea9070e75166de
Aysmaria <56933749+Aysmaria@users.noreply.github.com>
2023-07-26 20:03:24 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -264,37 +264,31 @@ if user_name:
             # Save the updated data to the worksheet
             save_data("sorted_FINAL_DATA", st.session_state['user_name'], user_data)
 
-            # Increment index or show completion message when all texts have been processed
+            # Increment index
             if 'selected_index' not in st.session_state:
                 st.session_state['selected_index'] = 0  # or any other default value
             elif st.session_state['selected_index'] < len(user_data) - 1:
                 st.session_state['selected_index'] += 1
-            else:  # When you reach the last text
-                if 'processed_last' in st.session_state and st.session_state['processed_last']:
-                    st.session_state['all_processed'] = True  # Update all_processed when all texts have been processed.
-                    st.write("All texts have been processed :) You are a hero! 🚀")
-                else:
-                    st.session_state['processed_last'] = True
-                    st.session_state[
-                        'selected_index'] += 1  # Still increment the index to trigger the opinion asking part
+            elif st.session_state['selected_index'] == len(user_data) - 1:
+                st.session_state['selected_index'] += 1
 
             for criterion in criteria:
                 st.session_state[criterion] = 0
                 st.session_state['comment'] = ''
                 st.session_state['category'] = categories[0]
 
-        if st.session_state['all_processed'] == True:
+        # Outside the "Next" button click event
+        if 'selected_index' in st.session_state and st.session_state['selected_index'] == len(user_data):
             user_opinion = st.text_area(
                 "Please share your overall experience. What do you think about the generated summaries?")
             submit_button = st.button("Submit")
             if submit_button:
                 user_data.at[
                     st.session_state[
-                        'selected_index'], 'User Opinion'] = user_opinion if user_opinion else "No opinion given"
+                        'selected_index'] - 1, 'User Opinion'] = user_opinion if user_opinion else "No opinion given"
                 # Save the updated data to the worksheet
                 save_data("sorted_FINAL_DATA", st.session_state['user_name'], user_data)
-                  # Update all_processed when user's opinion has been submitted
+                # Update all_processed when user's opinion has been submitted
                 st.write("All data is saved :)  Thank you for participation 🩷")
                 st.balloons()  # Streamlit balloons
                 st.stop()
-
