75d3a1abd4a81a524e7e1748e8e35a8f56e5ca38
Aysmaria <56933749+Aysmaria@users.noreply.github.com>
2023-07-26 13:26:46 +0200

    Add files via upload

--- App.py
+++ App.py
@@ -268,26 +268,21 @@ if user_name:
                 st.session_state['selected_index'] += 1
             elif st.session_state['selected_index'] == len(user_data) - 1:
                 if 'processed_last' in st.session_state and st.session_state['processed_last']:
-                    st.write("All texts have been processed :) You are a hero! 🚀")
+                    # Ask for the user's opinion after the last text has been processed
                     user_opinion = st.text_area(
-                        "Please share your overall experience. What do you think about the generated summaries?")
-                    submit_button = st.button("Submit")
+                        "All texts have been processed :) You are a hero! 🚀 Please share your overall experience. What do you think about the generated summaries?")
+                    submit_button = st.button("Submit Opinion", key='submit_opinion')
                     if submit_button:
-                        user_data.at[
-                            st.session_state[
-                                'selected_index'], 'User Opinion'] = user_opinion if user_opinion else "No opinion given"
+                        user_data.at[st.session_state[
+                                         'selected_index'], 'User Opinion'] = user_opinion if user_opinion else "No opinion given"
                         # Save the updated data to the worksheet
                         save_data("sorted_FINAL_DATA", st.session_state['user_name'], user_data)
-
                         st.session_state[
                             'all_processed'] = True  # Update all_processed when user's opinion has been submitted
-                        st.write("All data is saved :)  Thank you for participation 🩷")
+                        st.write("All data is saved :) Thank you for participation 🩷")
                         st.balloons()  # Streamlit balloons
                         st.stop()
-
-
-
-                 else:
+                else:
                     st.session_state['processed_last'] = True
 
             for criterion in criteria:
